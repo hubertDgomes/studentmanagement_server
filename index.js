07e9600a60e4eb33fe0e7afdb8b8cfe6980392c7
hubertDgomes <gomeshubert8@gmail.com>
2026-04-26 14:10:20 +0600

    chore: update CORS origin to production frontend URL

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin : "http://localhost:3000",
+    origin : "https://studentmanagement-nf1z6crzh-hubert-gomes-projects.vercel.app",
     credentials : true
 }))
 
